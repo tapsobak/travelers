f6eecf64ec7797a748f89e98fac02733b5e247e5
Nasser Tapsoba <nassertapsoba@meta.com>
2026-02-14 07:09:29 -0500

    Added a new *distance* function in the main file and called it in the calls file.

--- main_function_calls.py
+++ main_function_calls.py
@@ -1,6 +1,9 @@
 from travelers_main import *
 
-
+towns_loaded = load_towns("towns.txt")
 print("List of Towns and X & Y Coords Below!!!\n")
-print(load_towns("towns.txt"))
-print(f"Total numbers of towns: {len(load_towns("towns.txt"))}")
+print(towns_loaded)
+print(f"\nTotal numbers of towns: {len(towns_loaded)}")
+print(
+    f"\nEuclian Distance between {towns_loaded[0][0]} and {towns_loaded[1][0]} is: {distance(towns_loaded[0], towns_loaded[1])}"
+)
--- travelers_main.py
+++ travelers_main.py
@@ -1,7 +1,9 @@
 import os
+import math
 
 
 def load_towns(path):
+    """Simple function to load the list of towns from a text file."""
     dir = "../traveler_project/data"
     full_path = os.path.join(dir, path)
     if os.path.exists(full_path):
@@ -15,5 +17,17 @@ def load_towns(path):
         raise FileNotFoundError(f"Missing file: {path}")
 
 
+def distance(townA, townB):
+    """Calculate Euclidean Distance Between 2 towns and return the result"""
+
+    x1, y1 = float(townA[1]), float(townA[2])  # Get the X, Y coords for town A
+    x2, y2 = float(townB[1]), float(townB[2])  # Get the X, Y coords for town B
+
+    result = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    return round(result, 3)
+
+
 if "__name__" == "__main__":
     load_towns("towns.txt")
+    distance(load_towns("towns.txt")[0], load_towns("towns.txt")[1])
