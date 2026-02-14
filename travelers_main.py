ee820a3e80811acc680089cfd8594895bf56b320
Nasser Tapsoba <nassertapsoba@meta.com>
2026-02-13 22:49:09 -0500

    Added an execption to the load_towns function

--- travelers_main.py
+++ travelers_main.py
@@ -4,12 +4,15 @@ import os
 def load_towns(path):
     dir = "../traveler_project/data"
     full_path = os.path.join(dir, path)
-    with open(full_path, "r") as file:
-        contents = file.read().splitlines()  # creating a list of 'Town, X, Y'
-        town_details = [
-            tuple(content.split()) for content in contents
-        ]  # convertint the string 'Town X, Y' into a tuple ('Town, X, Y')
-        return town_details
+    if os.path.exists(full_path):
+        with open(full_path, "r") as file:
+            contents = file.read().splitlines()  # creating a list of 'Town, X, Y'
+            town_details = [
+                tuple(content.split()) for content in contents
+            ]  # convertint the string 'Town X, Y' into a tuple ('Town, X, Y')
+            return town_details
+    else:
+        raise FileNotFoundError(f"Missing file: {path}")
 
 
 if "__name__" == "__main__":
