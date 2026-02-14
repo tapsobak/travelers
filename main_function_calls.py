ca77c6e43f88e4db3dbde75953d8ebcbf6c57791
Nasser Tapsoba <nassertapsoba@meta.com>
2026-02-13 22:50:28 -0500

    Created a new function main_function_call.py to call the functions in main

---
+++ main_function_calls.py
@@ -0,0 +1,6 @@
+from travelers_main import *
+
+
+print("List of Towns and X & Y Coords Below!!!\n")
+print(load_towns("towns.txt"))
+print(f"Total numbers of towns: {len(load_towns("towns.txt"))}")
